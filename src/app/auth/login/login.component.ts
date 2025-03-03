39938aa8c228a21b8e538fca26e0b46ded9f7954
Ricsius <hajnal.ricsi@gmail.com>
2025-03-03 21:53:05 +0100

    Add form validation to template

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule]
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(formData: NgForm) {
+    const values = formData.form.value;
+    const email = values.email;
+    const password = values.password;
+  }
+}
